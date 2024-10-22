f1e1e22f720932a67564e569f29fc8e9a625448a
Tercius <mr.terks@yandex.ru>
2024-10-22 13:27:35 +0300

    update

--- frontend/src/app/pages/home/components/section10/section10.component.ts
+++ frontend/src/app/pages/home/components/section10/section10.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { naming } from "../../../../data/naming/naming/naming.service";
 import { PriceFormComponent } from "../price-form/price-form.component";
-import { NgIf } from "@angular/common";
+import { AsyncPipe, NgForOf, NgIf } from "@angular/common";
+import { SettingsService } from "../../../../stores/SettingsService";
+import { MarkdownComponent } from "ngx-markdown";
 
 @Component( {
   selector: 'app-section10',
   standalone: true,
   imports: [
     PriceFormComponent,
-    NgIf
+    NgIf,
+    NgForOf,
+    AsyncPipe,
+    MarkdownComponent
   ],
   templateUrl: './section10.component.html',
   styleUrl: './section10.component.scss'
 } )
 export class Section10Component {
+  constructor( public settings: SettingsService ) {
+  }
 
   priceForm: boolean = false;
 
@@ -23,4 +30,5 @@ export class Section10Component {
 
   naming = naming;
 
+  protected readonly console = console;
 }
--- frontend/src/app/pages/home/components/section11/section11.component.ts
+++ frontend/src/app/pages/home/components/section11/section11.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import {naming} from "../../../../data/naming/naming/naming.service";
+import { naming } from "../../../../data/naming/naming/naming.service";
+import { SettingsService } from "../../../../stores/SettingsService";
+import { MarkdownComponent } from "ngx-markdown";
+import { AsyncPipe } from "@angular/common";
 
-@Component({
+@Component( {
   selector: 'app-section11',
   standalone: true,
-  imports: [],
+  imports: [
+    MarkdownComponent,
+    AsyncPipe
+  ],
   templateUrl: './section11.component.html',
   styleUrl: './section11.component.scss'
-})
+} )
 export class Section11Component {
+  constructor( public settings: SettingsService ) {
+  }
 
   naming = naming;
 
