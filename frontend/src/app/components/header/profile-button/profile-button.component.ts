43a8af94c12973c1fd992974e28208dfae72ac75
Tercius <mr.terks@yandex.ru>
2024-11-12 13:20:25 +0300

    update

--- frontend/src/app/components/header/profile-button/profile-button.component.ts
+++ frontend/src/app/components/header/profile-button/profile-button.component.ts
@@ -1,16 +1,18 @@
-import {Component} from '@angular/core';
-import {naming} from "../../../data/naming/naming/naming.service";
-import {CustomSelectComponent} from "../../candidate/customSelect/customSelect.component";
+import { Component } from '@angular/core';
+import { naming } from "../../../data/naming/naming/naming.service";
+import { CustomSelectComponent } from "../../candidate/customSelect/customSelect.component";
+import { RouterLink } from "@angular/router";
 
-@Component({
+@Component( {
   selector: 'app-profile-button',
   standalone: true,
   imports: [
-    CustomSelectComponent
+    CustomSelectComponent,
+    RouterLink
   ],
   templateUrl: './profile-button.component.html',
   styleUrl: './profile-button.component.scss'
-})
+} )
 export class ProfileButtonComponent {
 
   naming = naming;
